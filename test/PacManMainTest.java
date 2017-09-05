48ac922165501762a5743cdb6995e0d903dccb40
John Meyer <0x326@users.noreply.github.com>
2017-09-04 20:14:36 -0400

    Add sample files

---
+++ test/PacManMainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// See the JUnit 5 user guide for info on making tests
+// http://junit.org/junit5/docs/current/user-guide/
+class PacManMainTest {
+
+    @Test
+    void sampleTest() {
+        assertTrue(2 + 3 == 5, "Write a string describing what is wrong if this test fails." +
+            "In this case, the laws of mathematics are not observed!");
+    }
+
+}
