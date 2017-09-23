2b11602cde748068a8c65cd902ef4df6dc3d2e00
John Meyer <0x326@users.noreply.github.com>
2017-09-23 16:38:33 -0400

    Redo IntelliJ/Eclipse project files and switch to JUnit 4

--- test/PacManMainTest.java
+++ test/PacManMainTest.java
@@ -1,15 +1,15 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-// See the JUnit 5 user guide for info on making tests
-// http://junit.org/junit5/docs/current/user-guide/
-class PacManMainTest {
+// See the JUnit 4 user guide for info on making tests
+// https://github.com/junit-team/junit4/wiki/Getting-started
+public class PacManMainTest {
 
     @Test
-    void sampleTest() {
-        assertTrue(2 + 3 == 5, "Write a string describing what is wrong if this test fails." +
-            "In this case, the laws of mathematics are not observed!");
+    public void sampleTest() {
+        assertTrue("Write a string describing what is wrong if this test fails." +
+            "In this case, the laws of mathematics are not observed!", 2 + 3 == 5);
     }
 
 }
