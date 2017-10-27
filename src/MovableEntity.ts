4a775580769ba6553e4eb04f9588990cfc3b41b6
John Meyer <0x326@users.noreply.github.com>
2017-10-27 17:24:56 -0400

    Unimplement Drawable
    MovableEntity's already know where they are.  ``location`` and ``neighboringEntities`` parameters are not useful

--- src/MovableEntity.ts
+++ src/MovableEntity.ts
@@ -8,7 +8,7 @@ import Drawable from './Drawable';
  *
  * @author Noah Dirig, Laurel Sexton, Gauthier Kelly, John Meyer
  */
-abstract class MovableEntity implements Drawable {
+abstract class MovableEntity {
 
     logicalLocation: [number, number];
 
@@ -45,12 +45,10 @@ abstract class MovableEntity implements Drawable {
      * Draw this object on the graphic at the given location.
      *
      * @param board               The graphic to draw on
-     * @param location            The location at which to draw
      * @param maxSize             The maximum size of the image.
      *                            The image drawn should be proportional to mazSize to support scaling.
-     * @param neighboringEntities A collection of adjacent entities.
      */
-    draw(board: CanvasRenderingContext2D, location: [number, number], maxSize: number, neighboringEntities: Drawable[]) {
+    draw(board: CanvasRenderingContext2D, maxSize: number) {
 
     }
 }
