eedf03c868cb4612df7134e0ccb051a0038caa89
John Meyer <0x326@users.noreply.github.com>
2017-11-18 16:49:05 -0500

    Add draft for UI states

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import * as React from 'react';
 import './App.css';
 import Board from './Board';
 
+enum GameState {
+  TitleScreen,
+  PlayingGame,
+  GameOver,
+  ListScores
+}
+
 interface Props {
 }
 
 interface State {
+  gameState: GameState;
   score: number;
 }
 
@@ -13,32 +21,97 @@ class App extends React.Component<Props, State> {
   constructor() {
     super();
     this.state = {
-      score: 0
+      gameState: GameState.TitleScreen,
+      score: 0,
     };
   }
 
   render() {
-    return (
-      <div className="App">
-        <div className="header">
-          Score: {this.state.score}
+    if (this.state.gameState === GameState.TitleScreen) {
+      return (
+        <div className="App">
+          <title>
+            Pac-Man
+          </title>
+          <div>
+            <button>
+              Start
+            </button>
+          </div>
         </div>
-        <div className="board">
-          <Board
-            width={'500 cm'}
-            height={'500 cm'}
-            active={true}
-            onScoreChange={(newScore: number) => this.setState({
-              score: newScore
-            })}
-            onGameFinish={() => {
+      );
+    } else if (this.state.gameState === GameState.PlayingGame) {
+      return (
+        <div className="App">
+          <div className="header">
+            Score: {this.state.score}
+          </div>
+          <div className="center">
+            <div className="board">
+              <Board
+                width="500 cm"
+                height="500 cm"
+                active={true}
+                onScoreChange={(newScore: number) => this.setState({
+                  score: newScore
+                })}
+                onGameFinish={() => {
 
-            }}
-          />
+                }}
+              />
+            </div>
+          </div>
+          <div className="footer">
+            <button>
+              Quit Game
+            </button>
+          </div>
         </div>
-        <div className="footer" />
-      </div>
-    );
+      );
+    } else if (this.state.gameState === GameState.GameOver) {
+      return (
+        <div className="App">
+          <div className="header">
+            Game Over
+          </div>
+          <div className="center">
+            <div>
+              Your score is {this.state.score}
+            </div>
+            <div>
+              Play Again?
+            </div>
+          </div>
+          <div className="footer">
+            <button>
+              Play Again
+            </button>
+            <button>
+              Quit
+            </button>
+          </div>
+        </div>
+      );
+    } else if (this.state.gameState === GameState.ListScores) {
+      return (
+        <div className="App">
+          <div className="header">
+            Game Over
+          </div>
+          <div className="center">
+            {/* TODO: Add scores here */}
+          </div>
+          <div className="footer">
+            <button>
+              Quit
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      // Note: This should never happen
+      return null;
+    }
   }
 }
 
