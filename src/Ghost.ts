1bd8bf270f3b78c6572fbd2a55b003a4d01b45ac
John Meyer <0x326@users.noreply.github.com>
2017-11-18 19:58:05 -0500

    Add vulnerability indicator to ghosts

--- src/Ghost.ts
+++ src/Ghost.ts
@@ -53,6 +53,38 @@ abstract class Ghost extends MovableEntity {
     this.state = VulnerabilityState.Dangerous;
   }
 
+  /**
+   * Draw this object on the graphic at the given location.
+   *
+   * @param board         The graphic to draw on
+   * @param maxSize       The maximum size of the image.
+   *              The image drawn should be proportional to mazSize to support scaling.
+   */
+  draw(board: CanvasRenderingContext2D, maxSize: number) {
+    super.draw(board, maxSize);
+    let drawLocation: [number, number] = [
+      this.logicalLocation[0] * maxSize - maxSize,
+      this.logicalLocation[1] * maxSize - maxSize
+    ];
+
+    switch (this.state) {
+      case VulnerabilityState.Vulnerable:
+        board.fillStyle = '#03A9F4';
+        break;
+
+      case VulnerabilityState.VulnerableBlinking:
+        board.fillStyle = '#FF9800';
+        break;
+
+      default:
+        board.fillStyle = '#F44336';
+        break;
+    }
+    board.beginPath();
+    board.arc(drawLocation[0], drawLocation[1], maxSize / 3, 0, 2 * Math.PI);
+    board.fill();
+  }
+
 }
 
 enum VulnerabilityState {
