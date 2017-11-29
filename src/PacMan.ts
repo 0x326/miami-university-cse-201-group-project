03d9e69a541c8b33a5ac1818e44c33e440814ed6
ndirig <31777263+ndirig@users.noreply.github.com>
2017-11-29 16:09:25 -0500

    Pacman animation
    Pacman sprite now displays.  Pacman is now animated while he moves.  Image does not rotate yet.

--- src/PacMan.ts
+++ src/PacMan.ts
@@ -2,6 +2,19 @@ import MovableEntity, { Direction } from './MovableEntity';
 import Drawable from './Drawable';
 import KeyboardListener from './KeyboardListener';
 
+// Pacman's animation sprites
+
+// Trying to only use three instead of nine
+const HalfOpen = require('./Pacman_halfopen_right.png');
+// const HalfOpenLeft = require('./Pacman_halfopen_left.png');
+//const HalfOpenUp = require('./Pacman_halfopen_up.png');
+// const HalfOpenDown = require('./Pacman_halfopen_down.png');
+const FullyOpen = require('./Pacman_fullyopen_right.png');
+// const FullyOpenLeft = require('./Pacman_fullyopen_left.png');
+//const FullyOpenUp = require('./Pacman_fullyopen_up.png');
+// const FullyOpenDown = require('./Pacman_fullyopen_down.png');
+const Closed = require('./Pacman_closed.png');
+
 /**
  * Course: CSE 201 A
  * Instructor: Dr. Kiper
@@ -19,6 +32,11 @@ class PacMan extends MovableEntity {
     'd': Direction.East
   };
 
+    // used for determining what animation sprite to display
+    static frameCount : number;
+    // instance variable, initializes the sprite
+    private sprite = new Image();
+
   /**
    * Creates a MovableEntity
    *
@@ -26,6 +44,7 @@ class PacMan extends MovableEntity {
    */
   constructor(initialLocation: [number, number], keyboardListener: KeyboardListener) {
     super(initialLocation);
+    PacMan.frameCount = 0;
     this.direction = Direction.North;
     for (let key in PacMan.KeyMap) {
       keyboardListener.registerKey(key, (isPressed: boolean) => {
@@ -37,6 +56,60 @@ class PacMan extends MovableEntity {
         }
       });
     }
+  }
+
+/**
+   * Draw this object on the graphic at the given location.
+   *
+   * @param board         The graphic to draw on
+   * @param maxSize       The maximum size of the image.
+   *              The image drawn should be proportional to mazSize to support scaling.
+   */
+  draw(board: CanvasRenderingContext2D, maxSize: number) {
+    // super.draw(board, maxSize);
+    let drawLocation: [number, number] = [
+      this.logicalLocation[0] * maxSize - maxSize,
+      this.logicalLocation[1] * maxSize - maxSize
+    ];
+      
+      
+      if (PacMan.frameCount <= 5) {
+          this.sprite.src = HalfOpen;
+      }
+      else if (PacMan.frameCount <= 10) {
+          this.sprite.src = FullyOpen;
+
+      }
+      else if (PacMan.frameCount <= 15) {
+          this.sprite.src = HalfOpen;
+      }
+      else {
+          this.sprite.src = Closed;
+      }
+      
+      /* Not sure how to rotate sprite
+      if (this.direction === Direction.North) {
+          this.sprite.style.transform = 'initial';
+          this.sprite.style.transform = 'rotate(90deg)';
+      }
+      else if (this.direction === Direction.South) {
+          this.sprite.style.transform = 'initial';
+          this.sprite.style.transform = 'rotate(270deg)';
+      }
+      else if (this.direction === Direction.West) {
+          this.sprite.style.transform = 'initial';
+          this.sprite.style.transform = 'rotate(180deg)';
+      }
+      */
+                   
+      if (!this.stopped) {
+          PacMan.frameCount++;
+      }
+      if (PacMan.frameCount === 20) {
+          PacMan.frameCount = 0;
+      }
+      
+    board.drawImage(this.sprite, (drawLocation[0] - (maxSize / 2)), (drawLocation[1] - (maxSize / 2)), maxSize, maxSize);
   }
 
   chooseDirection(map: Drawable[][]): void {
