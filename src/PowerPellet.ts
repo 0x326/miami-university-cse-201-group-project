95304a8d15fe01bb1114e39e41e876022914f564
John Meyer <0x326@users.noreply.github.com>
2017-12-01 17:16:05 -0500

    Refactor powerpellet draw

--- src/PowerPellet.ts
+++ src/PowerPellet.ts
@@ -9,9 +9,6 @@ import Drawable, { Neighbors } from './Drawable';
  * @author Noah Dirig, Laurel Sexton, Gauthier Kelly, John Meyer
  */
 class PowerPellet implements Drawable {
-  // used for determining what animation sprite to display
-  private frameCount = 0;
-
   /**
    * Draw this object on the graphic at the given location.
    *
@@ -22,18 +19,11 @@ class PowerPellet implements Drawable {
    * @param neighboringEntities A collection of adjacent entities.
    */
   draw(board: CanvasRenderingContext2D, location: [number, number], maxSize: number, neighboringEntities: Neighbors): void {
-
-    if (this.frameCount <= 10) {
+    if (performance.now() % 400 > 200) {
       board.fillStyle = '#673AB7';
       board.beginPath();
       board.arc(location[0], location[1], maxSize / 2, 0, 2 * Math.PI);
       board.fill();
-    } else {
-      board.clearRect(location[0], location[1], maxSize / 2, 0);
-    }
-    this.frameCount++;
-    if (this.frameCount === 20) {
-      this.frameCount = 0;
     }
   }
 }
